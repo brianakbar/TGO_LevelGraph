f05e29ee3da00a1b2bbd8a8b3ec5e74ae6a37c31
Brian Akbar <brianakbar@yahoo.com.sg>
2023-04-27 01:20:26 +0700

    Finish working on geometric representation converter

--- Assets/Scripts/Level Graph/GeometricRepresentationConverter.cs
+++ Assets/Scripts/Level Graph/GeometricRepresentationConverter.cs
@@ -5,11 +5,14 @@ namespace LevelGraph {
 
     [ExecuteInEditMode]
     public class GeometricRepresentationConverter : MonoBehaviour {
-        [SerializeField] List<GameObject> walls;
-        [SerializeField] List<GameObject> floors;
+        // [SerializeField] List<GameObject> walls;
+        // [SerializeField] List<GameObject> floors;
+        [SerializeField] List<GameObject> rooms;
+        [SerializeField] List<GameObject> corridors;
         [SerializeField] List<GameObject> doors;
         [SerializeField] VertexRepresentation vertexRepresentation;
         [SerializeField] EdgeRepresentation edgeRepresentation;
+        [SerializeField] float scale = 1f;
 
         [Space]
         [SerializeField] Editor editor;
@@ -31,6 +34,7 @@ namespace LevelGraph {
         class EdgeRepresentation {
             public float width;
             public float height;
+            public float step = 1f;
         }
 
         void Awake() {
@@ -38,8 +42,65 @@ namespace LevelGraph {
         }
 
         public void Convert() {
-            
+            GameObject level = new GameObject("Level");
+            foreach(Transform child in transform) {
+                if(child.TryGetComponent<Vertex>(out Vertex vertex)) {
+                    // GameObject box = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    // box.transform.position = vertex.transform.position;
+                    // box.transform.localScale = vertexRepresentation.size;
+                    GameObject room = Instantiate(PickRandomGameObject(rooms), vertex.transform.position, 
+                                                    Quaternion.identity, level.transform);
+                    room.transform.localScale *= scale;
+                }
+            }
+            foreach(Edge edge in edgeList.GetEdges()) {
+                Vector3 source = edge.GetTargetPosition();
+                Vector3 target = edge.GetSourcePosition();
+                int times = (int)(Vector3.Distance(target, source) / edgeRepresentation.step);
+                for(int i = 0; i < times; i++) {
+                    Quaternion rotation = Quaternion.LookRotation(target - source, Vector3.up);
+                    GameObject corridor = Instantiate(PickRandomGameObject(corridors), source, 
+                                                        rotation, level.transform);
+                    corridor.transform.localPosition += corridor.transform.rotation * Vector3.forward * edgeRepresentation.step * i;
+                    corridor.transform.localScale *= scale;
+                }
+                
+            }
         }
+
+        // List<GameObject> CreateCSGGameObject(List<GameObject> gameObjects) {
+        //     List<GameObject> CSGGameObjects = new List<GameObject>();
+        //     foreach(GameObject gameObject in gameObjects) {
+        //         GameObject prefabInstance = Instantiate(gameObject);
+        //         GameObject gameObjectUnion = null;
+        //         GameObject firstGameObject = null;
+        //         MeshFilter meshFilter = null;
+        //         MeshRenderer meshRenderer = null;
+        //         foreach(MeshFilter filter in prefabInstance.GetComponentsInChildren<MeshFilter>()) {
+        //             if(gameObjectUnion == null) {
+        //                 gameObjectUnion = new GameObject();
+        //                 meshFilter = gameObjectUnion.AddComponent<MeshFilter>();
+        //                 meshRenderer = gameObjectUnion.AddComponent<MeshRenderer>();
+        //                 firstGameObject = filter.gameObject;
+        //                 continue;
+        //             }
+        //             Model result;
+        //             if(firstGameObject != null) {
+        //                 result = CSG.Union(firstGameObject, filter.gameObject);
+        //                 firstGameObject = null;
+        //             }
+        //             else {
+        //                 result = CSG.Union(gameObjectUnion, filter.gameObject);
+        //             }
+
+        //             meshFilter.sharedMesh = result.mesh;
+        //             meshRenderer.sharedMaterials = result.materials.ToArray();
+        //         }
+        //         CSGGameObjects.Add(gameObjectUnion);
+        //         DestroyImmediate(prefabInstance);
+        //     }
+        //     return CSGGameObjects;
+        // }
 
         GameObject PickRandomGameObject(List<GameObject> gameObjects) {
             return gameObjects[Random.Range(0, gameObjects.Count)];
