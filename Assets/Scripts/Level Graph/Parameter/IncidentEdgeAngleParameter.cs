ba4a24906540b8a1fcdd68096278602e31e22db8
Brian Akbar <brianakbar@yahoo.com.sg>
2023-04-27 16:11:22 +0700

    Add edge generator parameter to check incident edge angle

--- Assets/Scripts/Level Graph/Parameter/IncidentEdgeAngleParameter.cs
+++ Assets/Scripts/Level Graph/Parameter/IncidentEdgeAngleParameter.cs
@@ -7,8 +7,32 @@ namespace LevelGraph.Parameter {
         [SerializeField] [Range(0, 180)] float max = 5;
 
         public override bool Check(Edge edge) {
-            
+            Vector3 edgeVector = GetVector(edge);
+            foreach(Edge incidentEdge in edge.GetSource().GetEdges()) {
+                if(incidentEdge == edge) continue;
+
+                Vector3 incidentEdgeVector = GetVector(incidentEdge);
+                float angleBetweenEdge = Vector3.Angle(edgeVector, incidentEdgeVector);
+
+                if(angleBetweenEdge < min || angleBetweenEdge > max) {
+                    return false;
+                }
+            }
+            foreach(Edge incidentEdge in edge.GetTarget().GetEdges()) {
+                if(incidentEdge == edge) continue;
+
+                Vector3 incidentEdgeVector = GetVector(incidentEdge);
+                float angleBetweenEdge = Vector3.Angle(edgeVector, incidentEdgeVector);
+
+                if(angleBetweenEdge < min || angleBetweenEdge > max) {
+                    return false;
+                }
+            }
             return true;
+        }
+
+        Vector3 GetVector(Edge edge) {
+            return edge.GetTarget().transform.position - edge.GetSource().transform.position;
         }
     }
 }
